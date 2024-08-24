396533fc15772d2f7949c58c4627ab66e10277b4
shakir245 <shakirshaki245@email.com>
2024-08-24 20:42:51 +0530

    changed

--- js/swiper-script.js
+++ js/swiper-script.js
@@ -1,34 +1,13 @@
-// $(function () {
-//     var swiper = new Swiper('.swiper', {
-//         autoplay: {
-//             delay: 1000
-//         },
-//         speed: 2000,
-//         slidesPerView: 1,
-//         slidesPerGroup: 3,
-//         spaceBetween: 30,
-//         loop: false,
-//         breakpoints: {
-//             // when window width is >= 360px
-//             360: {
-//                 slidesPerView: 1,
-//             },
-//             // when window width is >= 768px
-//             768: {
-//                 slidesPerView: 1,
-//             },
-//             // when window width is >= 1024px
-//             1024: {
-//                 slidesPerView: 1,
-//             }
-//         },
-//         // If we need pagination
-//         pagination: {
-//             enabled: false,
-//             el: '.swiper-pagination',
-//             type: 'bullets',
-//             clickable: true,
-//         },
-//     });
-
-// });
+document.addEventListener('DOMContentLoaded', function () {
+    const swiper = new Swiper('.swiper', {
+        loop: true,  // Optional: loops the slides
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    });
+});
